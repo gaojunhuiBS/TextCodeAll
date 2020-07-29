23e7c93add2d32c6ff93facd7da678f22245b194
275907983@qq.com <275907983@qq.com>
2020-07-29 16:53:21 +0800

    角标

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.badge.BadgeActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        bageBtn.setOnClickListener {
+            BadgeActivity.start(this)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/badge/BadgeActivity.kt
+++ app/src/main/java/com/example/myapplication/badge/BadgeActivity.kt
@@ -1,13 +1,28 @@
 package com.example.myapplication.badge
 
-import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_badge.*
 
 /**
  * 应用角标
  */
-class BadgeActivity : Activity() {
+class BadgeActivity : AppCompatActivity() {
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, BadgeActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_badge)
+        addBtn.setOnClickListener {
+            BadgeUtils.setCount(10, this)
+        }
     }
 }
